65e0070215a12d50673402e64a87ddc1ef9a86e4
Tara Sabella <tarasabella@gmail.com>
2018-11-17 19:35:07 +0000

    Done.

--- simple_looping.rb
+++ simple_looping.rb
@@ -1,8 +1,6 @@
 # REMEMBER: print your output to the terminal using 'puts'
 
 def loop_iterator(number_of_times)
-  loop do while number_of_times < 7 
-    puts "Welcome to the Flatiron School's Web Development Course!"
   # Code your solution here using the "loop" keyword to puts out the below phrase
   # This and all the other methods take an argument of an integer 
   # The integer is the number of times the loops should puts out the phrase
@@ -12,36 +10,55 @@ def loop_iterator(number_of_times)
   #  phrase and break when the counter hits the appropriate number...
   phrase = "Welcome to Flatiron School's Web Development Course!"
   
+  counter = 0
+  loop do 
+    puts phrase
+    counter += 1
+    break if counter == number_of_times
+  end
+
 end
 
 def times_iterator(number_of_times)
-  7.times do 
-puts  "Welcome to Flatiron School's Web Development Course!"
-end
+  # code your solution here using the "times" keyword
+  phrase = "Welcome to Flatiron School's Web Development Course!"
   
+  number_of_times.times do 
+    puts phrase 
+  end
 end
 
 def while_iterator(number_of_times)
-  counter = 0 
-  while counter < 7 
-  puts "Welcome to Flatiron School's Web Development Course!"
-end
+  # code your solution here using the "while" keyword
+  # hint: user a counter to tell the while loop when to stop!
+  phrase = "Welcome to Flatiron School's Web Development Course!"
   
+  count = 1
+  while count <= number_of_times
+    puts phrase
+    count += 1
+  end
 end
 
 def until_iterator(number_of_times)
-  counter = 0 
-  until counter == 7 
-  puts "Welcome to Flatiron School's Web Development Course!"
-counter += 1 
-end
+  # code your solution here using the "until" keyword
+  # hint: use a counter to tell the until loop when to stop!
+  phrase = "Welcome to Flatiron School's Web Development Course!"
+
+  count = 1
+  until count > number_of_times
+    puts phrase
+    count += 1
+  end
 end
 
 def for_iterator(number_of_times)
- for counter in 1..7 do 
-   puts 
-   "Welcome to Flatiron School's Web Development Course!"
- end
+  # code your solution here using the "for" keyword
+  # remember that `for` requires a range of numbers. How can we turn our number into a range?
   
+  phrase = "Welcome to Flatiron School's Web Development Course!"
+  
+  for count in 1..number_of_times do
+    puts phrase
+  end
 end
-
